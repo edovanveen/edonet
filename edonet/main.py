Grad checked conv layer, fixed bug in conv backprop.

edonet/main.py
@@ -154,8 +154,7 @@ class Conv2DLayer:
         dloss_dx = np.tensordot(dloss_dz, dz_dx, axes=((1, 2, 3), (1, 3, 5)))
         dloss_dx = dloss_dx[:, self.padding[1][0]:self.padded_size[0] - self.padding[1][1],
                             self.padding[2][0]:self.padded_size[1] - self.padding[2][1], :]
-        dloss_dw = np.average(dloss_dw, axis=0)
-        dloss_db = np.average(dloss_dw, axis=(0, 1))
+        dloss_db = np.average(dloss_dw, axis=(0, 1, 2))
 
         # Update weights.
         if update_weights:

gradient_check.py
@@ -7,12 +7,14 @@ import edonet
 def main():
     
     # Make input data.
-    x = np.random.rand((1, 6, 6, 3))
+    np.random.seed(0)
+    x = np.random.rand(1, 2, 2, 2)
+    y = [[0, 1]]
     
     # Make and train model.
-    model = edonet.NeuralNet(input_size=(6, 6, 1),
+    model = edonet.NeuralNet(input_size=(2, 2, 2),
                              layers=({'type': 'conv2D', 'nr_filters': 2, 'filter_size': (3, 3),
-                                      'activation': 'relu', 'stride': (1, 1), 'padding': 'valid'},
+                                      'activation': 'relu', 'stride': (1, 1), 'padding': 'same'},
                                      {'type': 'maxpool', 'pool_size': (2, 2)},
                                      {'type': 'flatten'},
                                      {'type': 'dense', 'nr_nodes': 4, 'activation': 'relu'},
@@ -22,14 +24,54 @@ def main():
                              seed=0)     
     
     # Calculate derivative of loss with respect to output of last layer.
-    y_pred = self.predict(x)
-    dloss_dx = self.loss_d(y_pred, y_true)
+    y_pred = model.predict(x)
+    dloss_dx = model.loss_d(y_pred, y)
     
     # Do backpropagation and store derivatives.
-    for layer in self.layers[::-1]:
-        dloss_dx = layer.back_prop(dloss_dx, learning_rate, 
-                                   update_weights=True, store_derivatives=True)
+    for layer in model.layers[::-1]:
+        dloss_dx = layer.back_prop(dloss_dx, 0, False, True)
 
-
+    # Check derivatives of loss wrt inputs x.
+    epsilon = 1e-3
+    dloss_dx_check = np.zeros(dloss_dx.shape)
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                x_check0 = x.copy()
+                x_check1 = x.copy()
+                x_check0[0, i, j, k] = x[0, i, j, k] - epsilon
+                x_check1[0, i, j, k] = x[0, i, j, k] + epsilon
+                y_check0 = model.predict(x_check0)
+                y_check1 = model.predict(x_check1)
+                dloss_dx_check[0, i, j, k] = (model.loss(y_check1, y)[0] - 
+                                              model.loss(y_check0, y)[0]) / (2 * epsilon)
+    print("dloss_dx:")
+    print(dloss_dx[0])
+    print("")
+    print(dloss_dx_check[0])
+    print("---------------")
+    
+    # Check derivatives of loss wrt weights of conv2d layer.
+    dloss_dw = model.layers[0].dloss_dw
+    orig_filters = model.layers[0].filters.copy()
+    dloss_dw_check = np.zeros(dloss_dw.shape)
+    for h in range(2):
+        for i in range(3):
+            for j in range(3):
+                for k in range(2):
+                    model.layers[0].filters = orig_filters.copy()
+                    model.layers[0].filters[h, i, j, k] = orig_filters[h, i, j, k] - epsilon
+                    y_check0 = model.predict(x)
+                    model.layers[0].filters[h, i, j, k] = orig_filters[h, i, j, k] + epsilon
+                    y_check1 = model.predict(x)
+                    dloss_dw_check[h, i, j, k] = (model.loss(y_check1, y)[0] - 
+                                                  model.loss(y_check0, y)[0]) / (2 * epsilon)
+    print("dloss_dw of conv layer:")
+    print(dloss_dw[0])
+    print("")
+    print(dloss_dw_check[0])
+    print("---------------")
+    
+        
 if __name__ == "__main__":
     main()