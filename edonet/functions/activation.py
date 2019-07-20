Fixed error in softmax, accuracy now 93%.

edonet/functions/activation.py
@@ -93,7 +93,7 @@ def softmax(z):
        Output values.
     """
     
-    expz = np.subtract(np.exp(z), np.amax(z, axis=1, keepdims=True))
+    expz = np.exp(z)
     return np.divide(expz, np.sum(expz, axis=1, keepdims=True))
 
 

edonet/functions/loss.py
@@ -19,7 +19,7 @@ def cel(y_pred, y_true):
        Loss values per example.
     """
     
-    epsilon = 1e-15
+    epsilon = 1e-16
     return -1. * np.sum(np.multiply(y_true, np.log(np.maximum(y_pred, epsilon))), axis=1)
 
 

edonet/main.py
@@ -481,7 +481,7 @@ class NeuralNet:
         for layer in self.layers[::-1]:
             dloss_dx = layer.back_prop(dloss_dx, learning_rate)
     
-    def fit(self, x, y_true, epochs=1, learning_rate=0.1, batch_size=100):
+    def fit(self, x, y_true, epochs=1, learning_rate=0.1, batch_size=100, verbose=False):
         """
         Fit neural net to training set x, y_true
 
@@ -497,6 +497,8 @@ class NeuralNet:
             Learning rate of the network. Default: 0.1.
         batch_size : int, optional
             Batch size. Default: 100.
+        verbose : bool, optional
+            Print update for each batch. Default: False
         """
         
         # Calculate number of batches.
@@ -519,6 +521,10 @@ class NeuralNet:
                 
                 # Calculate average loss.
                 avg_loss[i] = np.average(self.loss(y_pred, y_batch))
+                
+                # Print status.
+                if verbose:
+                    print("- Batch: ", i, "/", nr_batches, ", loss: ", avg_loss[i])
                 
                 # Backpropagation and gradient descent.
                 self.grad_desc(y_pred, y_batch, learning_rate)

test_conv.py
@@ -13,7 +13,7 @@ def make_dataset():
     encoder = np.eye(10, dtype=int)
     y_train = np.array([encoder[i] for i in y_train])
     y_test = np.array([encoder[i] for i in y_test])
-    return x_train[::50], x_test[::50], y_train[::50], y_test[::50]
+    return x_train, x_test, y_train, y_test
 
 
 # Check accuracy.
@@ -30,21 +30,23 @@ def main():
     # Make dataset.
     x_train, x_test, y_train, y_test = make_dataset()
     
-    # Make and train model.
+    # Make model.
     model = edonet.NeuralNet(input_size=(28, 28, 1),
-                             layers=({'type': 'conv2D', 'nr_filters': 16, 'filter_size': (3, 3),
+                             layers=({'type': 'conv2D', 'nr_filters': 20, 'filter_size': (3, 3),
                                       'activation': 'relu', 'stride': (1, 1), 'padding': 'same'},
                                      {'type': 'maxpool', 'pool_size': (2, 2)},
-                                     {'type': 'conv2D', 'nr_filters': 16, 'filter_size': (3, 3),
+                                     {'type': 'conv2D', 'nr_filters': 10, 'filter_size': (3, 3),
                                       'activation': 'relu', 'stride': (1, 1), 'padding': 'same'},
                                      {'type': 'maxpool', 'pool_size': (2, 2)},
                                      {'type': 'flatten'},
-                                     {'type': 'dense', 'nr_nodes': 200, 'activation': 'relu'},
-                                     {'type': 'dense', 'nr_nodes': 50, 'activation': 'tanh'},
+                                     {'type': 'dense', 'nr_nodes': 32, 'activation': 'relu'},
+                                     {'type': 'dense', 'nr_nodes': 16, 'activation': 'relu'},
                                      {'type': 'dense', 'nr_nodes': 10, 'activation': 'softmax'}),
                              loss='CEL',
                              seed=0)
-    model.fit(x_train, y_train, epochs=70, learning_rate=0.005, batch_size=50)
+
+    # Train model with decreasing learning rate.
+    model.fit(x_train, y_train, epochs=10, learning_rate=0.05, batch_size=500, verbose=True)
 
     # Show result on test set.
     print("test labels:")